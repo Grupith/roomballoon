d899a22cc7b733fa391e6c2e9b843a6afcb83825
Dylan Koss <dylanhkoss@gmail.com>
2023-09-03 21:37:28 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <main className="bg-slate-300 min-h-screen">
+      <h1 className="text-center text-3xl py-10">RoomBalloon</h1>
+    </main>
+  )
+}
