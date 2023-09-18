a533d78d44af0de6473eeccf5af0ea71e945e066
Dylan Koss <dylanhkoss@gmail.com>
2023-09-18 15:33:13 -0500

    redeploy

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function Dashboard() {
   return (
     <ProtectedPage>
       <main className="min-h-screen overflow-hidden bg-gradient-to-r from-slate-900 to-slate-700 pt-16">
-        <h1 className="text-white text-center text-2xl font-semibold pt-2">
+        <h1 className="text-white text-center text-2xl font-semibold pt-4">
           Dashboard
         </h1>
       </main>
