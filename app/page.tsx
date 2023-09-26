a1df6669f4010f1603d9f36db8fdda35943d32de
Dylan Koss <dylanhkoss@gmail.com>
2023-09-25 22:04:08 -0500

    updated next config

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 "use client"
-import Image from "next/image"
 import { useFirebase } from "./FirebaseContext"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
 
 export default function Home() {
   const router = useRouter()
-  const { signInWithGoogle, user } = useFirebase()
+  const { signInWithGoogle } = useFirebase()
 
   const handleSignIn = () => {
     signInWithGoogle()
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   distDir: "out",
   images: {
     domains: ["lh3.googleusercontent.com"],
